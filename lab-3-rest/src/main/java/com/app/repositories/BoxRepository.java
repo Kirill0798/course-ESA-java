fc6859c6f6d03467333b4341962a76faae96c7b3
Almaz Dabisov <dabisov.almaz@gmail.com>
2021-01-18 05:35:15 +0400

    Delete lab-3-rest directory

--- lab-3-rest/src/main/java/com/app/repositories/BoxRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.repositories;
-
-import com.app.entities.Box;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BoxRepository extends CrudRepository<Box, Long> {
-}
--- lab-3-rest/src/main/java/com/app/repositories/GiftRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.repositories;
-
-import com.app.entities.Gift;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GiftRepository extends CrudRepository<Gift, Long> {
-}
